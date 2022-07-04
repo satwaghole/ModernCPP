d2c61d160b028e4427d0113ec54c9d23ad0875fc
HumbleSwagger <39756792+satwaghole@users.noreply.github.com>
2022-07-04 13:26:42 +0530

    lambda function example

---
+++ lambda_function.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	/* demonstration of the lamda function */
+	auto my_lamda_func = []()->void{
+	cout<<"\n Hello world";
+	};
+	
+	my_lamda_func();
+}
